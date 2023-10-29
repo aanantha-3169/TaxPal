7f012374ed01d1d660543b7c3da35059b1c6ca4f
aanantha-3169 <aananmariappan@gmail.com>
2023-10-29 16:48:20 +0800

    uncomment message generation code

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ index = load_data()
 memory = ChatMemoryBuffer.from_defaults(token_limit=1500)
 
 if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-     st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question",
-     verbose=True)
+    st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question",
+    verbose=True)
 #  st.session_state.chat_engine = index.as_chat_engine(
 #     chat_mode="context",
 #     memory=memory,
@@ -62,11 +62,11 @@ for message in st.session_state.messages: # Display the prior chat messages
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-# # If last message is not from assistant, generate a new response
-# if st.session_state.messages[-1]["role"] != "assistant":
-#     with st.chat_message("assistant"):
-#         with st.spinner("Thinking..."):
-#             response = st.session_state.chat_engine.chat(prompt)
-#             st.write(response.response)
-#             message = {"role": "assistant", "content": response.response}
-#             st.session_state.messages.append(message) # Add response to message history
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
